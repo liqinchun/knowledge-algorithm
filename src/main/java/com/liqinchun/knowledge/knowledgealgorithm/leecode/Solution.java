0c6ec81b0c8eb3f948b63be90ab1908e7ad68371
liqinchun <liqc@inhand.com.cn>
2021-03-17 19:12:47 +0800

    提交leecode题1 两数求和

--- src/main/java/com/liqinchun/knowledge/knowledgealgorithm/leecode/Solution.java
+++ src/main/java/com/liqinchun/knowledge/knowledgealgorithm/leecode/Solution.java
@@ -1,6 +1,32 @@
 package com.liqinchun.knowledge.knowledgealgorithm.leecode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+
+    /**
+     * 题 1 给定一个整数数组nums和一个整数目标值target，请你在数组中找出和为目标值的两个数，并返回他们的下标
+     * 两数之和
+     */
+    public static int[] twoSum(int [] arr,int target) {
+
+        Map<Integer,Integer> map = new HashMap();
+
+        for (int i = 0;i <arr.length;i++) {
+//            if (map.get(arr[i]) == (target - arr[i]))
+            if (map.containsKey(target - arr[i]))
+                return new int[] {i,map.get(target - arr[i])};
+            map.put(arr[i],i);
+        }
+
+        return new int[0];
+    }
+    /**
+     * 题2
+     *
+     */
 
     /**
      * 验证回文字符串
@@ -57,5 +83,11 @@ public class Solution {
         System.out.println("----------------------------------");
         System.out.println(validPalindrome1("asddsaa"));
         System.out.println(validPalindrome2("asddsssa"));
+        int array[] = new int[]{3,2,5,1};
+        int target = 5;
+        int[] arrSum = twoSum(array,target);
+        for (int a:arrSum) {
+            System.out.println(a);
+        }
     }
 }
